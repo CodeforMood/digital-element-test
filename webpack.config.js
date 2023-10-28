2a2885aeb398fb133faccbfbe88ed577bca3dd66
CodeMachine <98046355+CodeforMood@users.noreply.github.com>
2023-10-28 09:30:32 +0000

    Подготавливает проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -12,6 +13,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'template.html'),
       filename: 'index.html',
+    }),
+    new FileManagerPlugin({
+      events: {
+          onStart: {
+          delete: ['dist'],
+        },
+      },
     }),   
   ],
   devServer: {
@@ -19,5 +27,33 @@ module.exports = {
     port: 8080,
     compress: true,
     allowedHosts: "all", 
+  },
+  module: {
+   rules: [
+       {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+      {
+      test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+        filename: path.join('icons', '[name].[contenthash][ext]'),
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
   },
 };
